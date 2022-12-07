2873ad05fa9014db2a05f96df6175dc8d5964985
Antzelo <antzelo.balliu@rokoko.com>
2022-12-07 21:44:39 +0200

    updated tailwind imports and setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { Inter } from '@next/font/google';
+import type { AppProps } from 'next/app';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <style jsx global>
+      {`
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+        }
+      `}
+    </style>
+    <QueryClientProvider client={queryClient}>
+      <Component className={`${inter.variable} font-sans`} {...pageProps} />
+    </QueryClientProvider>
+  </> 
 }
