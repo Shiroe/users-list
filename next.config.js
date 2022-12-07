920da4ca1c59bf4ca6c4b0258514b0bcb79c89d0
Antzelo <antzelo.balliu@rokoko.com>
2022-12-07 21:43:06 +0200

    updated setup configs and packages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,42 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.generated.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uifaces.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    {
+      pattern: /(bg|text)-(red|green|blue|purple|orange|pink|gray|brand)-(20|30|40|50|60|70|80|90|100)/,
+    },
+  ],
+  theme: {
+    fontSize: {
+      sm: ['12px', '18px'],
+      base: ['14px', '21px'],
+      lg: ['16px', '24px'],
+      xl: ['18px', '27px'],
+    },
+    backgroundColor: {
+      'gray-100': '#EDF2F7',
+      'brand-50': '#413EEF',
+      'purple-20': '#EFE2FE',
+      'purple-80': '#574195',
+      'pink-20': '#FEDDEB',
+      'pink-80': '#710C36',
+      'blue-20': '#CEF1FD',
+      'blue-80': '#04546F',
+      'orange-20': '#FEEBC8',
+      'orange-80': '#91472C',
+      'gray-90': '#171A22',
+      'gray-80': '#2D3748',
+      'gray-70': '#4A5568',
+      'gray-60': '#718096',
+      'gray-50': '#A0AEC0',
+      'gray-40': '#CBD5E0',
+      'gray-30': '#E2E8F0',
+      'gray-20': '#EDF2F7',
+    },
+    colors: {
+      'brand-50': '#413EEF',
+      'purple-20': '#EFE2FE',
+      'purple-80': '#574195',
+      'pink-20': '#FEDDEB',
+      'pink-80': '#710C36',
+      'blue-20': '#CEF1FD',
+      'blue-80': '#04546F',
+      'orange-20': '#FEEBC8',
+      'orange-80': '#91472C',
+      'gray-90': '#171A22',
+      'gray-80': '#2D3748',
+      'gray-70': '#4A5568',
+      'gray-60': '#718096',
+      'gray-50': '#A0AEC0',
+      'gray-40': '#CBD5E0',
+      'gray-30': '#E2E8F0',
+      'gray-20': '#EDF2F7',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    }
+  },
+  plugins: [],
+}
